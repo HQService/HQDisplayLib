8a7bb9172e0daa0f55bb666a19846c25b9866be9
cccgh5 <47964442+cccgh5@users.noreply.github.com>
2023-08-27 23:49:32 +0900

    Initial commit

---
+++ modules/dist/build.gradle.kts
@@ -0,0 +1,21 @@
+import resourcegenerator.bukkit.excludedRuntimeDependencies
+
+plugins {
+    id("hq.shared")
+    id("hq.shadow")
+    id("hq.resource-generator")
+}
+
+bukkitResourceGenerator {
+    main = "kr.hqservice."
+    name = ""
+    apiVersion = "1.13"
+    libraries = excludedRuntimeDependencies()
+}
+
+dependencies {
+    compileOnly(libs.spigot.api)
+    compileOnly(framework.core)
+    runtimeOnly(project(":modules:core"))
+    runtimeOnly(project(":modules:api"))
+}
